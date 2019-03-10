8010b33ee7fa431ca372f22d8c2ab52bf4f78d3e
Alson Shareef <alson.shareef@gmail.com>
2019-03-10 18:34:46 +0800

    Added todo list

---
+++ todo.js
@@ -0,0 +1,7 @@
+/* TODO LIST */
+
+/*
+    1. Start planning out game that I want to create, and how I am going to go about it.
+        - Code structure, daily and weekly goals, starting features to begin working on, and any extra
+        concepts I will need to know in order to create the game.
+*/
