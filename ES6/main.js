c25d8ed419035f91e76165cf4e8c418440c2c4cd
Alson Shareef <alson.shareef@gmail.com>
2019-03-13 21:28:45 +0800

    Installed Babel and updated todo

--- ES6/main.js
+++ ES6/main.js
@@ -1,3 +1,4 @@
 
 // ** This file is responsible for connecting all the controllers, and essentially run the general app logic. ** //
 
+
--- todo.js
+++ todo.js
@@ -2,7 +2,8 @@
 
 /*
     1. Start planning out game that I want to create, and how I am going to go about it.
-        - Setup babel to be able to use ES6 syntax for writing modular JS code
+        - Setup Babel to be able to use ES6 syntax for writing modular JS code (DONE)
+        - Setup Webpack
         - Start building the logic for creating a player and how the player is going to move through user input
         within the defined world.
 */
