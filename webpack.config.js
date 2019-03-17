b220015ff95cddf08cd2d8f74f15231fb4c705c4
Alson Shareef <alson.shareef@gmail.com>
2019-03-17 16:44:54 +0800

    Added content hash to main.js file name for efficient cache busting, and HTML Webpack plugin for generating build/index.html with hashed main.js file added dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path');
 
+// Plugin used to generate build/index.html with hashed main.js file linked dynamically
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+// **
+
 module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'main.[contentHash].js',
         path: path.resolve(__dirname, 'build')
     },
+    plugins: [new HTMLWebpackPlugin({
+        template: "./src/template.html" // HTML template that will be used to generate build/index.html
+    })],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
                     "style-loader", // 2. Injects styles into DOM through main.js.
-                     "css-loader" // 1. Turns CSS into CommonJS.
+                    "css-loader" // 1. Turns CSS into CommonJS.
                 ], 
             },
             {
