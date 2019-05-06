a14d39ae1a1683a227fab3c32221db99c1183e01
Alson Shareef <alson.shareef@gmail.com>
2019-05-06 19:17:35 +0800

    Refactored some of the original Game class properties or methods and added setupEventListeners and start methods.

--- src/models/game.js
+++ src/models/game.js
@@ -1,90 +1,72 @@
-// ** This file is responsible for storing all game related logic. ** //
+/**
+ * File: game.js
+ * Description: The Game class is the 'controller' which governs all the interactions between components of the game.
+ */
 
-export class Game {
+import Display from "./display";
+import Engine from "./engine";
+import World from "./world";
+import Control from "./control";
+
+export default class Game {
 	constructor() {
-		// This object will hold properties related to the world that the character will exist in.
-		this.world = {
-			background_color: 'rgb(17, 60, 81)',
-			friction: 0.95,
-			gravity: 0,
-			player: new Player(),
-			height: 108,
-			width: 192,
-			handleCollision: (player) => {
-				// If player hits max-left, stop player from continuing left.
-				if (player.x < 0) {
-					player.x = 0;
-					player.velocity_x = 0;
-				}
-
-				// If player hits max-right, stop player from continuing right.
-				if (player.x + player.width > this.world.width) {
-					player.x = this.world.width - player.width;
-					player.velocity_x = 0;
-				}
-
-				// If player hits max-bottom, stop player from continuing down.
-				if (player.y < 0) {
-					player.y = 0;
-					player.velocity_y = 0;
-				}
-
-				// If player hits max-height, stop player from continuing up.
-				if (player.y + player.height > this.world.height) {
-					player.y = this.world.height - player.height;
-					player.velocity_y = 0;
-				}
-			},
-			update: function() {
-				this.player.velocity_x *= this.friction;
-				this.player.velocity_y *= this.friction;
-				this.player.update();
-				this.handleCollision(this.player);
-			}
-		};
+		this.display = new Display(document.getElementById("primary-canvas"));
+		this.engine = new Engine(this.update, this.draw);
+		this.world = new World();
+		this.player = this.world.player;
+		this.control = new Control();
 	}
 
-	updateWorld = () => {
+	// Resizes canvas whenever viewport dimensions change, to dimensions with same aspect ratio as game world.
+	resize = () => {
+		this.display.resizeCanvas(
+			window.innerHeight - 30,
+			window.innerWidth - 30,
+			this.world.height / this.world.width
+		);
+		this.display.drawWorld();
+	};
+
+	// Fills the buffer canvas with the world, then draws buffer onto primary canvas.
+	draw = () => {
+		this.display.fillBuffer(this.world.background_color);
+		this.display.drawPlayer(
+			this.player.x,
+			this.player.y,
+			this.player.width,
+			this.player.height,
+			this.player.color
+		);
+		this.display.drawWorld();
+	};
+
+	// Updates the state of the player and world based on user input.
+	update = delta => {
+		if (this.control.left.active) {
+			this.player.moveLeft(delta);
+		}
+		if (this.control.right.active) {
+			this.player.moveRight(delta);
+		}
+		if (this.control.up.active) {
+			this.player.moveUp(delta);
+		}
+		if (this.control.down.active) {
+			this.player.moveDown(delta);
+		}
 		this.world.update();
 	};
-}
 
-class Player {
-	constructor(x, y) {
-		this.color = 'rgb(244, 182, 66)';
-		this.height = 15;
-		this.width = 15;
-		this.jumping = true;
-		this.velocity_x = 0;
-		this.velocity_y = 0;
-		this.x = 100;
-		this.y = 50;
-	}
-
-	moveLeft = (delta) => {
-		this.velocity_x -= 0.05 * delta;
+	// Setup event listeners for window resizes and user input.
+	setupEventListeners = () => {
+		window.addEventListener("resize", this.resize);
+		window.addEventListener("keydown", this.control.keyListener);
+		window.addEventListener("keyup", this.control.keyListener);
 	};
 
-	moveRight = (delta) => {
-		this.velocity_x += 0.05 * delta;
-	};
+	// INITIALIZE
 
-	moveUp = (delta) => {
-		this.velocity_y -= 0.05 * delta;
-	};
-
-	moveDown = (delta) => {
-		this.velocity_y += 0.05 * delta;
-	};
-
-	// jump = () => {
-	// 	if (!this.jumping) {
-	// 		this.jumping = true;
-	// 	}
-	// };
-
-	update = () => {
-		this.x += this.velocity_x;
-		this.y += this.velocity_y;
+	start = () => {
+		requestAnimationFrame(this.engine.loop);
 	};
 }
