5ece26db538c3b4e330dffd56f96419e78f2bc6e
Alson Shareef <alson.shareef@gmail.com>
2019-05-07 19:43:25 +0800

    Renamed Control class to Input to make the purpose of the class more obvious

--- src/models/input.js
+++ src/models/input.js
@@ -1,10 +1,11 @@
 /**
- * File: control.js
+ * File: input.js
  * Description: This file handles the user input.
  */
 
-// The Control class holds instances of the Action class which will store state of keypresses, specifically if they're activated or not.
-export default class Control {
+// The Input class holds instances of the Action class which will store the keypress state of different keys, specifically if they're activated or not.
+
+export default class Input {
 	constructor() {
 		this.left = new Action();
 		this.right = new Action();
@@ -12,6 +13,7 @@ export default class Control {
 		this.up = new Action();
 	}
 
+	// Listens for keypresses and changes the state of respective keys accordingly.
 	keyListener = e => {
 		let pressed = e.type === "keydown" ? true : false;
 
