c7bd8151c5e853b3b6715352a4fb5da2277a8db3
Alson Shareef <alson.shareef@gmail.com>
2019-05-08 17:43:15 +0800

    Removed need for buffer, so no need to set buffer dimensions to world dimensions.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ import Game from "./models/game";
 
 let game = new Game();
 
-window.addEventListener("resize", () => {
-	console.log("Height:", game.world.height);
-	console.log("Width:", game.world.width);
-});
-
-// Set buffer dimensions to game world dimensions so buffer is drawn correctly onto canvas.
-// game.display.buffer.canvas.height = game.world.height;
-// game.display.buffer.canvas.width = game.world.width;
-
 // GAME INITIALIZATION //
 game.setupEventListeners();
 game.resize(); // Resizes canvas to viewport dimensions on first load
