a25ec80373755e3aad528c6d21f5aaa485599768
Alson Shareef <alson.shareef@gmail.com>
2019-05-08 17:49:35 +0800

    Updated resize method to invoke updateWorldSize method

--- src/models/game.js
+++ src/models/game.js
@@ -22,17 +22,20 @@ export default class Game {
 
 	// Resizes canvas whenever viewport dimensions change, to dimensions with same aspect ratio as game world.
 	resize = () => {
-		this.display.resizeCanvas(
+		// New canvas dimensions variable after window resize.
+		let worldSize = this.display.resizeCanvas(
 			window.innerHeight - 30,
 			window.innerWidth - 30,
 			this.world.height / this.world.width
 		);
+
+		// Update the world size to stay proportionate to new canvas dimensions and then redraw.
+		this.world.updateWorldSize(worldSize.width, worldSize.height);
 		this.display.drawWorld();
 	};
 
 	// Fills the buffer canvas with the world, then draws buffer onto primary canvas.
 	draw = () => {
-		// this.display.fillBuffer(this.world.background_color);
 		this.display.drawWorld(this.world.background_color);
 		this.display.drawPlayer(
 			this.player.x,
