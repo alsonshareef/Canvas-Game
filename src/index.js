8cfc912f6b631fa73bc8b5860a4a1c65c86427a2
Alson Shareef <alson.shareef@gmail.com>
2019-05-09 19:39:34 +0800

    Added high level description of the Game Flow and how the toggling of game states work.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@
  * Description: This file is responsible for all game initialization duties and setting up event listeners for user input.
  */
 
+/** GAME FLOW **
+ * 1. Starts off loading static default level data in 'game' mode.
+ * 2. Player may decide to either:
+ * 		- Go into map mode:
+ * 			- to build a custom map, save it and play in game mode.
+ * 			- to load/edit a custom map, save it and play in game mode.
+ * 			- to delete an existing custom map.
+ */
+
+/** Toggling Modes
+ * Identify what methods should be invoked regardless of state.
+ * if (game.mode.play) > setup event listeners for playing the game.
+ * if (game.mode.map) > setup event listeners for editing/loading/saving/deleting level data.
+ */
+
 import "./index.css";
 import Game from "./models/game";
 
