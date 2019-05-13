fa84ca13a06c253c49e44be3c283e1849d5c4e90
Alson Shareef <alson.shareef@gmail.com>
2019-05-13 19:52:40 +0800

    Added blocks.js to store the different element classes that can be drawn

---
+++ src/models/blocks.js
@@ -0,0 +1,8 @@
+// This file stores all of the different element classes and their props.
+export class solidElement {
+	constructor(width, height, position) {
+		this.width = width;
+		this.height = height;
+		this.position = position;
+	}
+}
